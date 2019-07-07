318120b6d9ecc1022f895e6865dd60ea70ea6b41
JosephPR <reilly4181@gmail.com>
2019-07-07 10:49:31 -0600

    Updates

---
+++ precache-manifest.20085aef0fa3a43e033498204c0a9c05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ee79c0a30fc50ef506de2024b36a34",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8915867768e9a0a23ecb",
+    "url": "/monsters-rolodex/static/css/main.5d8f9783.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8915867768e9a0a23ecb",
+    "url": "/monsters-rolodex/static/js/main.9d2dd9a2.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
